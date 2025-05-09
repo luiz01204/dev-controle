7dab4f6f0256129cd19471709229e3912b23cff8
luiz01204 <luiz.machado.santos2000@gmail.com>
2025-05-09 19:34:17 -0300

    update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import hero from "../../public/hero.svg"
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-[calc(100vh-80px)] px-4 mx-auto">
-      <h2 className="font-medium text-2xl mb-2">Gerencia seus atendimentos.</h2>
-      <h2 className="font-bold text-3xl text-blue-500 mb-8 md:text-4xl">Melhore seu tempo de resposta.</h2>
+      <h2 className="font-medium text-2xl mb-2 text-center">Gerencia seus atendimentos.</h2>
+      <h2 className="font-bold text-3xl text-blue-500 mb-8 md:text-4xl text-center">Melhore seu tempo de resposta.</h2>
       <Image 
         src={hero}
         alt="hero da pagina inicial"
